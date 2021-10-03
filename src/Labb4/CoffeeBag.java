b74c56d193025fa04ba36c5aef3c45e46c2f0381
mgniguse <91496765+mgniguse@users.noreply.github.com>
2021-10-03 15:14:00 -0500

    10/03/2021

---
+++ src/Labb4/CoffeeBag.java
@@ -0,0 +1,36 @@
+package Labb4;
+
+import java.util.Scanner;
+
+public class CoffeeBag {
+
+        public static void main(String []args){
+
+            double totalPrice = 0;
+            double bagWeight=0;
+            double numberOfBags =0;
+            double pricePerLb=5.99;
+            double totalPriceWithTax=0;
+            double taxrate=7.25;
+            Scanner in = new Scanner(System.in);
+            System.out.print("Enter the number of bags sold: ");
+            numberOfBags=in.nextDouble();
+            System.out.print("Enter weight per a bag: ");
+            bagWeight=in.nextDouble();
+
+            System.out.println("Price per a pound: $5.99");
+            System.out.println("Sales tax: 7.25%");
+
+            totalPrice = bagWeight * numberOfBags * pricePerLb;
+
+            totalPriceWithTax = totalPrice + totalPrice * taxrate/100;
+
+            totalPriceWithTax= Math.round(totalPriceWithTax * 100.0) / 100.0;
+
+            System.out.println("Total Price: "+ totalPriceWithTax);
+
+
+        }
+    }
+
+
---
+++ src/Labb4/WeightConverter.java
@@ -0,0 +1,30 @@
+package Labb4;
+
+import java.util.Scanner;
+
+    public class WeightConverter {
+        private double moonWeight;
+
+        public WeightConverter(double moonWeight) {
+            this.moonWeight = moonWeight;
+        }
+
+        public double convert(double earthWeight) {
+            double weight = earthWeight * 0.167;
+            return weight;
+
+        }
+
+        public static void main(String[] args) {
+            WeightConverter moonWeight = new WeightConverter(0.167);
+
+            Scanner msgie = new Scanner(System.in);
+            System.out.println("Enter your weight");
+            double earthweight;
+            System.out.println("Your moon weight is: " +moonWeight.convert(earthweight=msgie.nextDouble()));
+            msgie.close();
+
+        }
+    }
+
+
